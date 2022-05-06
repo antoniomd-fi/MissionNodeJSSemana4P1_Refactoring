5996c24abbd8c37678c68f85b0be56abf575950a
Antonio Martin <90221784+antoniomd-fi@users.noreply.github.com>
2022-05-05 22:20:56 -0500

    Feat: Explorer Service

---
+++ refactoring/lib/services/ExplorerService.js
@@ -0,0 +1,18 @@
+class ExplorerService{
+    static filterByMission(explorers, mission){
+        const explorersInMission = explorers.filter((explorer) => explorer.mission == mission);
+        return explorersInMission
+    }
+
+    static getAmountOfExplorersByMission(explorers,mission){
+        const explorersInMission = explorers.filter((explorer) => explorer.mission == mission);
+        return explorersInMission.length
+    }
+
+    static getExplorersUsernamesByMission(explorers,mission){
+        const explorersInMission = explorers.filter((explorer) => explorer.mission == mission);
+        const explorersUsernames = explorersInMission.map((explorer)=>explorer.githubUsername)
+        return explorersUsernames
+    }
+}
+module.exports= ExplorerService
