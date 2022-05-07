cf50fb6f2d501e6edb9fbcce88ad4bf72b86f6ba
Antonio Martin <90221784+antoniomd-fi@users.noreply.github.com>
2022-05-06 21:42:27 -0500

    Update ExplorerController Test

--- test/controllers/ExplorerController.test.js
+++ test/controllers/ExplorerController.test.js
@@ -1,7 +1,7 @@
 const ExplorerController = require("./../../lib/controllers/ExplorerController");
 const ExploreController= require("./../../lib/controllers/ExplorerController");
 
-describe ("Tests for ExploreService", () =>{
+describe ("Tests for ExplorerController", () =>{
 
     test ("1. Filter By Mission" , ()=> {
         const explorersInMission = ExploreController.getExplorersByMission("node")
@@ -15,15 +15,15 @@ describe ("Tests for ExploreService", () =>{
 
     test ("2. Amount of Explorers" , ()=> {
         const explorersAmount = ExploreController.getExplorersAmountByMission("java");
-        expect(explorersAmount.length).toBe(5);
+        expect(explorersAmount).toBe(5);
     });
 
     test ("3. Explorers usernames" , ()=> {
         const explorersInMission = ExplorerController.getExplorersUsernamesByMission("java")
-        expect(explorersInMission[0].githubUsername).toBe("ajolonauta6");
-        expect(explorersInMission[1].githubUsername).toBe("ajolonauta7");
-        expect(explorersInMission[2].githubUsername).toBe("ajolonauta8");
-        expect(explorersInMission[3].githubUsername).toBe("ajolonauta9");
-        expect(explorersInMission[4].githubUsername).toBe("ajolonauta10");
+        expect(explorersInMission[0]).toBe("ajolonauta6");
+        expect(explorersInMission[1]).toBe("ajolonauta7");
+        expect(explorersInMission[2]).toBe("ajolonauta8");
+        expect(explorersInMission[3]).toBe("ajolonauta9");
+        expect(explorersInMission[4]).toBe("ajolonauta10");
     });
 });
